95d09a0f65d4ee372486740849371b6a4032ad29
Aleksis Daugats <daugatsa@gmail.com>
2022-06-13 21:37:24 +0300

    atlicis izdomat jautajumus

--- src/Testins.java
+++ src/Testins.java
@@ -2,24 +2,21 @@ import java.util.Scanner;
 
 public class Testins {
 	
-	static int minesana(int pareizie, String atbilde){
-		Scanner dati = new Scanner(System.in);
-		System.out.print("Minējums: ");
-		String minejums = dati.next().toLowerCase();
+	static int minesana(int pareizie, String atbilde, String minejums){
+		
+		
 		if(minejums.equals(atbilde)){
-			System.out.println("Pareizi!");
 			pareizie++;
-		}else{
-			System.out.println("Nepareizi!");
 		}
-		dati.close();
+		
 
 		return pareizie;
 	}
 	
 	public static void main(String[] args) {
+		Scanner dati = new Scanner(System.in);
 		int pareizie=0;
-		String atbilde;
+		String atbilde, minejums;
 		
 		System.out.println("\tTESTS");
 		System.out.println("Noteikumi\n-Jāizvēlas 2 līdz 3 atbildes\n-Atbildes ievadīt alfabētiskā secībā");
@@ -30,81 +27,103 @@ public class Testins {
 							+"\nc) 3"
 							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
-		
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
+
 		System.out.println("\n2. Testa jautajums"
 							+"\na) 1"
 							+"\nb) 2"
 							+"\nc) 3"
 							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 		
 		System.out.println("\n3. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 		
 		System.out.println("\n4. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 		
 		System.out.println("\n5. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 		
 		System.out.println("\n6. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 
 		System.out.println("\n7. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 
 		System.out.println("\n8. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 
 		System.out.println("\n9. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
 
 		System.out.println("\n10. Testa jautajums"
-				+"\na) 1"
-				+"\nb) 2"
-				+"\nc) 3"
-				+"\nd) 4");
+							+"\na) 1"
+							+"\nb) 2"
+							+"\nc) 3"
+							+"\nd) 4");
 		atbilde = "bc";
-		pareizie = minesana(pareizie,atbilde);
+		System.out.print("Minējums: ");
+		minejums = dati.next().toLowerCase();
+		pareizie = minesana(pareizie,atbilde,minejums);
+		
+		System.out.print("Tu atbildēji pareizi uz "+pareizie+" no 10 jautājumiem.");
 
-
+		dati.close();
 	}
 
 }
